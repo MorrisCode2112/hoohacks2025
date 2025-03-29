b6ec7fbcb238f79421fbcbcb4d1bb4273804675f
Josh <morri6jc@dukes.jmu.edu>
2025-03-29 19:53:47 -0400

    more frontend stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
-import { useState } from "react";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Meditate from "./Meditate";
+
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="container">
-      {/* Pulsating Background */}
-      <div className="pulsating-sphere"></div>
-
-      {/* Content */}
-      <div className="content">
-        <h1>Mental Zen</h1>
-        <p className="subtitle">
-          Click on the buttons below to improve your mental health with a
-          personalized AI!
-        </p>
-        <div className="button-container">
-          <a href="#">
-            <button className="button">Meditate With Zenny</button>
-          </a>
-          <a href="#">
-            <button className="button">Start New Chat</button>
-          </a>
-        </div>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/meditate" element={<Meditate />} />
+      </Routes>
+    </Router>
   );
 }
 
