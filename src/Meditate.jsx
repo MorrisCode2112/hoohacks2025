6f682f2e00254073d4e92dc5b4a37d9c92639507
Josh <morri6jc@dukes.jmu.edu>
2025-03-29 20:19:21 -0400

    frontend stuff

---
+++ src/Meditate.jsx
@@ -0,0 +1,13 @@
+function Meditate() {
+  var meditatetext = 'test';
+  return (
+    <div className="container">
+    {/* Pulsating Background */}
+      
+      <div className="pulsating-sphere-green"></div>
+        <h1>{meditatetext}</h1>
+    </div>
+  );
+}
+
+export default Meditate;
